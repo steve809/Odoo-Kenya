2d5c23cda449206940a3511369f9463b245dbc75
Sai-Raj-Ltd <57390445+Sai-Raj-Ltd@users.noreply.github.com>
2020-02-28 12:21:46 +0300

    itax_upload

---
+++ itax_upload/wizard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Part of Hyperthink Systems Limited. (Website: www.hyperthinkkenya.co.ke).
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+
+from . import vat_csv_report
