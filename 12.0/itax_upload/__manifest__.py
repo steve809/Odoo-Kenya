00d87ee11adf28a8a5fcc3f767aff4b6b840c7a1
steve809 <61575339+steve809@users.noreply.github.com>
2020-02-28 11:21:07 +0300

    Update __manifest__.py

--- 12.0/itax_upload/__manifest__.py
+++ 12.0/itax_upload/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Part of  Hyperthink Systems Limited. (Website: www.hyperthinkkenya.co.ke).
+# OCP. (Website: www.OCP.co.ke).
 # See LICENSE file for full copyright and licensing details.
 #
 ##############################################################################
