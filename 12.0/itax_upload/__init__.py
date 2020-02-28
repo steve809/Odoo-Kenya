a95cd8f09c8ad66398ed49782ae4701753dca965
steve809 <61575339+steve809@users.noreply.github.com>
2020-02-28 11:06:23 +0300

    Add files via upload

---
+++ 12.0/itax_upload/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Part of Hyperthink Systems Limited. (Website: www.ocp.co.ke).
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+
+from . import models
+from . import wizard
---
+++ 12.0/itax_upload/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Part of  Hyperthink Systems Limited. (Website: www.hyperthinkkenya.co.ke).
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+
+{
+    'name': 'VAT Report',
+    'version': '12.0.1.0',
+    'summary': 'VAT report for importing in iTAX',
+    'description': """
+    """,
+    'author': 'OCP',
+    'website': 'www.ocp.co,ke',
+    'category': 'Reports',
+    'depends': ['account'],
+    'data': [
+        
+        'wizard/vat_report_view.xml',
+        
+    ],
+    'installable': True,
+}
